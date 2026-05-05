221c5ea708146ec143311bdf51101f5343252f56
Assnsster <nghoangviet202@gmail.com>
2026-05-05 21:40:05 +0700

    Modify RDP wrapper installation process

--- run.ps1
+++ run.ps1
@@ -1,4 +1,5 @@
 Invoke-WebRequest "https://raw.githubusercontent.com/Assnsster/archiver1/refs/heads/main/RDPWInst.exe" -OutFile "$env:TEMP\RDPWInst.exe"
 Start-Process "$env:TEMP\RDPWInst.exe" -ArgumentList "-i -o" -Verb RunAs -Wait
 Invoke-WebRequest "https://raw.githubusercontent.com/Assnsster/archiver1/refs/heads/main/RDPConf.exe" -OutFile "$env:USERPROFILE\AppData\Local\Microsoft\WindowsApps\RDPConf.exe"
-Start-Process powershell -Verb RunAs -ArgumentList "-Command Invoke-WebRequest 'https://raw.githubusercontent.com/sebaxakerhtc/rdpwrap.ini/master/rdpwrap.ini' -OutFile 'C:\Program Files\RDP Wrapper\rdpwrap.ini'; Start-Sleep -Seconds 3"
+Start-Process powershell -Verb RunAs -ArgumentList "-Command Stop-Service -Name TermService -Force; Invoke-WebRequest 'https://raw.githubusercontent.com/sebaxakerhtc/rdpwrap.ini/master/rdpwrap.ini' -OutFile 'C:\Program Files\RDP Wrapper\rdpwrap.ini'; Start-Service -Name TermService ; Start-Sleep -Seconds 3"
+
